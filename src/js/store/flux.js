c8dfead3ff1a23ac9417993e317331b29aa27d93
gars150387 <gar.santeliz@gmail.com>
2021-07-26 15:04:18 +0000

    set addContact function

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -15,6 +15,28 @@ const getState = ({ getStore, setStore, getActiosn }) => {
 							});
 					})
 					.catch(e => console.error(e));
+			},
+			addContact(name, phone, email, address){
+				fetch( url,{
+					method: "POST",
+					headers: {"Content-type": "application/json"},
+					body: JSON.stringigy({
+						full_name: name,
+						phone: phone,
+						address: address,
+						agenda_slug: "gars_1503"
+					})
+				}).then(() =>{
+					fetch(url + "agenda/gars_1503")
+					.then(response => response.json())
+					.then( result => {
+						console.log("result", result),
+						setStore({
+							contacts: result
+						});
+					})
+					.catch(e=> console.error(e));
+				})
 			}
 		}
 	};
