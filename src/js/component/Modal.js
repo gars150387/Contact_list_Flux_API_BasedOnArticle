d5beb6a61c6fe25470dd9c8b0096ac7661e54b89
gars150387 <gar.santeliz@gmail.com>
2021-08-03 19:09:24 +0000

    set modal to return to main page after confirm action

--- src/js/component/Modal.js
+++ src/js/component/Modal.js
@@ -24,7 +24,7 @@ export const Modal = props => {
 						<h5 className="modal-title">Are you sure?</h5>
 						{props.onClose ? (
 							<button
-								onClick={() => props.onClose()}
+								onClick={() => props.onClose({ showModal: false })}
 								type="button"
 								className="close"
 								data-dismiss="modal"
