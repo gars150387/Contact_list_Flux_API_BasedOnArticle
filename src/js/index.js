777bb7448c3aa7d1a2e583da0c963747c05efc17
gars150387 <gar.santeliz@gmail.com>
2021-09-18 13:03:05 +0000

    finding issue of why data is not being shown

--- src/js/index.js
+++ src/js/index.js
@@ -15,20 +15,19 @@ import "../styles/index.scss";
 //import your own components
 import Layout from "./layout";
 
-
 var firebaseConfig = {
-    apiKey: "AIzaSyAGA2Mwg3UcZn9mxF2YyZOCoNvvc8CBGgc",
-    authDomain: "auth-test-fa7a8.firebaseapp.com",
-    projectId: "auth-test-fa7a8",
-    storageBucket: "auth-test-fa7a8.appspot.com",
-    messagingSenderId: "498887090970",
-    appId: "1:498887090970:web:9050dccf083cd5aa4d42a3",
-    measurementId: "G-EER0CEF6HG"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  firebase.auth();
+	apiKey: "AIzaSyAGA2Mwg3UcZn9mxF2YyZOCoNvvc8CBGgc",
+	authDomain: "auth-test-fa7a8.firebaseapp.com",
+	projectId: "auth-test-fa7a8",
+	storageBucket: "auth-test-fa7a8.appspot.com",
+	messagingSenderId: "498887090970",
+	appId: "1:498887090970:web:9050dccf083cd5aa4d42a3",
+	measurementId: "G-EER0CEF6HG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.auth();
 
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
